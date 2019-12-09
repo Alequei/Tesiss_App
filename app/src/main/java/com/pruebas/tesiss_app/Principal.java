b1825ec184cddcc15ab3208ac4c5cac432ac0ed7
Your NameDiego <diego.torres.h@tecsup.edu.pe>
2019-12-09 16:36:18 -0500

    v3

--- app/src/main/java/com/pruebas/tesiss_app/Principal.java
+++ app/src/main/java/com/pruebas/tesiss_app/Principal.java
@@ -5,17 +5,25 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class    Principal extends AppCompatActivity {
     List<Slider> sliderList;
     private ViewPager sliderpager;
+    private TabLayout indicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         sliderpager = findViewById(R.id.viewPager);
+        indicator = findViewById(R.id.indicator_id);
         sliderList = new ArrayList<>();
         sliderList.add(new Slider(R.drawable.servicio_corte,"Cortes"));
         sliderList.add(new Slider(R.drawable.servicio_maquillaje,"Maquillaje"));
@@ -25,9 +33,28 @@ public class    Principal extends AppCompatActivity {
 
         SliderAdapter adapter = new SliderAdapter(this,sliderList);
         sliderpager.setAdapter(adapter);
+        indicator.setupWithViewPager(sliderpager,true);
+
+        Timer time=new Timer();
+        time.scheduleAtFixedRate(new Principal.sliderTimer(),4000,6000);
+    }
+
+        class sliderTimer extends TimerTask{
+
+            @Override
+            public void run() {
+                Principal.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (sliderpager.getCurrentItem()<sliderList.size() - 1){
+                            sliderpager.setCurrentItem(sliderpager.getCurrentItem()+1);
+                        }else {
+                            sliderpager.setCurrentItem(0);
+                        }
+                    }
+                });
+            }
         }
-
-
     }
 
 
